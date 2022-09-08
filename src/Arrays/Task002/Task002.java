4fe5db72d4324f6267b189c97cbe88cfb9e4fd4e
Andrey <andrey.stolyarov1337@gmail.com>
2022-09-09 00:23:20 +0300

    NOT SOlVED

--- src/Arrays/Task002/Task002.java
+++ src/Arrays/Task002/Task002.java
@@ -4,34 +4,37 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class Task002 {
+    public static String[] cardSuit = {"Черви", "Буби", "Крести", "Пики"};
+    public static String[] cardSeniority = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "Валет", "Дама", "Король", "Туз"};
+    public static String[][] cardsOnHands = new String[5][1];
+
     public static void main(String[] args) {
-        String[] cardSuit = {"Черви", "Буби", "Крести", "Пики"};
-        String[] cardSeniority = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "Валет", "Дама", "Король", "Туз"};
+
         Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите допустимое количество игроков для игры:");
         int playersCount = scanner.nextInt();
-        if (playersCount > 5) System.out.println("Игроков больше пяти!");
-        int cardCount = 5;
+        if (playersCount > 5 || playersCount == 0 || playersCount == 1) {
+            System.out.println("Недопустимое количество игроков!");
+            return;
+        }
 
+        for (int i = 1; i <= playersCount; i++) {
+            System.out.println("У игрока " + i + " Карты: " + Arrays.deepToString(cardSorting()));
+        }
 
-        String[][] combinationOnHands = new String[cardCount][1];
-        String[][] combinationOnHands1 = new String[cardCount][1];
-        String[][] combinationOnHands2 = new String[cardCount][1];
-        String[][] combinationOnHand3 = new String[cardCount][1];
-        String[][] combinationOnHands4 = new String[cardCount][1];
+    }
 
-
+    public static String[][] cardSorting() {
         String[] randomCombination = new String[5];
-        for (int i = 0; i <= combinationOnHands.length-1; i++) {
+        for (int i = 0; i <= cardsOnHands.length - 1; i++) {
             for (int j = 0; j <= randomCombination.length - 1; j++) {
                 randomCombination[i] = cardSuit[(int) Math.round(Math.random() * (cardSuit.length - 1))]
                         + " " + cardSeniority[(int) Math.round(Math.random() * (cardSeniority.length - 1))];
             }
-            combinationOnHands[i][0] = randomCombination[i];
+            cardsOnHands[i][0] = randomCombination[i];
         }
-
-        for (int i = 1; i <= playersCount; i++) {
-            System.out.println("У игрока " + i + " на руках карты " + Arrays.deepToString(combinationOnHands));
-        }
+        return cardsOnHands;
     }
 }
+
 
