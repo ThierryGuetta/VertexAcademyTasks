778a6690aedafb2c31c2473900f1d0b3321348fc
Andrey <andrey.stolyarov1337@gmail.com>
2022-09-09 16:38:38 +0300

    add solution from VertexAcademy task 006

---
+++ src/Arrays/Task006/Task006.java
@@ -0,0 +1,24 @@
+package Arrays.Task006;
+
+import java.util.Arrays;
+
+public class Task006 {
+    public static void main(String[] args) {
+        int[] array = new int[4];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = (int) Math.floor(Math.random() * (99 - 10) * 10);
+        }
+        System.out.println(Arrays.toString(array));
+
+        int index = 0;
+        for (int i = 1; i < array.length; i++) {
+            if (array[i - 1] >= array[i]) index++;
+        }
+        if (index == 0) {
+            System.out.println("Строгая последовательность");
+        } else {
+            System.out.println("Не строгая последовательность");
+        }
+    }
+}
+
