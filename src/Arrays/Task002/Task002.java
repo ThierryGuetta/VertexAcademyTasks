de358a72fa951fc8388e677436beffda7327b246
Andrey <andrey.stolyarov1337@gmail.com>
2022-09-09 14:41:22 +0300

    add solution from VertexAcademy task 002

--- src/Arrays/Task002/Task002.java
+++ src/Arrays/Task002/Task002.java
@@ -4,24 +4,24 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class Task002 {
-    public static String[] cardSuit = {"Черви", "Буби", "Крести", "Пики"};
-    public static String[] cardSeniority = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "Валет", "Дама", "Король", "Туз"};
+    public static String[] cardSuit = {"Р§РµСЂРІРё", "Р‘СѓР±Рё", "РљСЂРµСЃС‚Рё", "РџРёРєРё"};
+    public static String[] cardSeniority = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "Р’Р°Р»РµС‚", "Р”Р°РјР°", "РљРѕСЂРѕР»СЊ", "РўСѓР·"};
     public static String[] randomCombination = new String[52];
 
     public static void main(String[] args) throws InterruptedException {
 
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Введите допустимое количество игроков для игры (от 2х до 10):");
+        System.out.println("Р’РІРµРґРёС‚Рµ РґРѕРїСѓСЃС‚РёРјРѕРµ РєРѕР»РёС‡РµСЃС‚РІРѕ РёРіСЂРѕРєРѕРІ РґР»СЏ РёРіСЂС‹ (РѕС‚ 2С… РґРѕ 10):");
         int playersCount = scanner.nextInt();
         if (playersCount > 10 || playersCount == 0 || playersCount == 1) {
-            System.out.println("Недопустимое количество игроков!");
+            System.out.println("РќРµРґРѕРїСѓСЃС‚РёРјРѕРµ РєРѕР»РёС‡РµСЃС‚РІРѕ РёРіСЂРѕРєРѕРІ!");
             return;
         }
-        System.out.println("Перемешиваем колоду...");
+        System.out.println("РџРµСЂРµРјРµС€РёРІР°РµРј РєРѕР»РѕРґСѓ...");
         Thread.sleep(2000);
         cardSorting();
 
-        System.out.println("Раздаем карты...");
+        System.out.println("Р Р°Р·РґР°РµРј РєР°СЂС‚С‹...");
         Thread.sleep(2000);
         cardDistribution(playersCount);
     }
@@ -53,7 +53,7 @@ public class Task002 {
     public static void cardDistribution(int playersCount) {
         int index = 0;
         for (int i = 1; i <= playersCount; i++) {
-            System.out.print("У игрока " + i + " карты: ");
+            System.out.print("РЈ РёРіСЂРѕРєР° " + i + " РєР°СЂС‚С‹: ");
             for (int j = 0; j < 5; j++) {
                 System.out.print(randomCombination[index] + ", ");
                 index++;
